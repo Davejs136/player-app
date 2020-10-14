ac8fd4c59640901bd631d21d666ca574bb8b1470
Davejs136 <davejs136@gmail.com>
2020-10-14 19:02:03 -0400

    add: Home html struct

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
 const Home = () => (
-  <p>Im Home page</p>
+  <section className="home">
+    <div className="home__header">
+      <h1>My Music</h1>
+      <input type="text" placeholder="search music" />
+    </div>
+    <div className="home__clasification">
+      <span className="home__mode home__mode--active">Songs</span>
+    </div>
+    <div className="home__view-mode">
+      <div className="iconm">Grid</div>
+      <div className="home__sort">
+        sort by: <span className="home__sort-active">Date added</span>
+      </div>
+    </div>
+    <div className="home__list">
+      <ul>
+        <li>List 1</li>
+        <li>List 2</li>
+        <li>List 3</li>
+        <li>List 4</li>
+        <li>List 5</li>
+      </ul>
+    </div>
+  </section>
 )
 
 export default Home
