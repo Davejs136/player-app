594a91b6474985013f2db73a7747e41a037c8335
Davejs136 <davejs136@gmail.com>
2020-10-14 17:26:01 -0400

    add: sidebar links

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from '../routes'
+import MenuLeft from '../components/MenuLeft'
 
 function Layout() {
   return (
     <Router>
+      <section className="menu-left">
+        <MenuLeft />
+      </section>
       <section className="main">
         <Routes />
       </section>
